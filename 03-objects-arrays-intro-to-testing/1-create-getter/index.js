3d4b68e73018ee4ee7afeef3fa76cdc8d006cd73
Boris Mukovnin <ninwim@gmail.com>
2024-03-07 00:28:44 +0300

    3.1 task createGetter function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,19 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const props = path.split('.');
 
+  return function (obj) {
+    let result = obj;
+
+    for (let prop of props) {
+      if (prop in result) {
+        result = result[prop];
+      } else {
+        return undefined;
+      }
+    }
+
+    return result;
+  }
 }
